5d4d01d3923e2f144e6790def350367be4366537
Felipe Lima <felipe.lima@gmail.com>
2015-09-23 23:42:15 -0700

    Fix assets hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  ActionController::Base.asset_host = 'http://104.131.134.208'
   config.action_controller.asset_host = 'http://104.131.134.208'
 
   # Ignore bad email addresses and do not raise email delivery errors.
