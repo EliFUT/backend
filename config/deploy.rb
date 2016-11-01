de164a44243c70645a0b747417aa622ea8b88241
Felipe Lima <felipe.lima@gmail.com>
2016-10-31 22:45:38 -0700

    More bower fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,3 +50,17 @@ namespace :deploy do
   end
 
 end
+
+namespace :bower do
+  desc 'Install bower'
+  task :install do
+    on roles(:web) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'bower:install'
+        end
+      end
+    end
+  end
+end
+before 'deploy:compile_assets', 'bower:install'
