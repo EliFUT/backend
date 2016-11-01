128fc94cb3cae68817e4cd1ee644f1c01cb06e44
Felipe Lima <felipe.lima@gmail.com>
2016-10-31 22:34:55 -0700

    Switch bower install method

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,17 +50,3 @@ namespace :deploy do
   end
 
 end
-
-namespace :bower do
-  desc 'Install bower'
-  task :install do
-    on roles(:web) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, 'bower:install CI=true'
-        end
-      end
-    end
-  end
-end
-before 'deploy:compile_assets', 'bower:install'
