fe807c902bf55fd71e7fa9fe1733610f75f191bd
Felipe Lima <felipe.lima@gmail.com>
2016-10-31 23:58:07 -0700

    Precompile bower assets

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module ElifutBackend
 
     config.assets.paths << Rails.root.join('assets')
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join('lib', 'assets', 'bower_components')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   end
 end
